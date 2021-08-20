d83236a0e9b5da7675366f6e4f94ae4f8b3cccca
HoaNghiem <hoanv@lifetimetech.vn>
2021-08-21 01:43:47 +0700

    Route Basic Part 1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# copy thu muc vendor and .env to run
+
+# https://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@github.com/hoa76001/php-laravel-learning.git
+
 # php-laravel-learning
--- laravelP1/routes/web.php
+++ laravelP1/routes/web.php
@@ -11,10 +11,21 @@
 |
 */
 
-Route::get('/Hoa', function () {
+Route::get('/', function () {
 	echo "hh";
     return view('welcome');
 });
-Route::get('/', function () {
-    echo "hh";
-});
+
+// truyen tham so tren route
+    Route::get('Route/{pram}', function ($pram) {
+        echo "Param: " . $pram;
+    });
+
+    // dat dieu kien cho tham so truyen len voi phuong thuc where()
+    Route::get('Route1/{date}', function ($date) {
+        echo "Date: " . $date;
+    })->where(['date'=>'[0-9]+']); //a-zA-Z
+// dinh danh va nhom cho Route
+    Route::get('Route2', ['as'=>'Route3',function () {
+        echo "This is Route2";
+    }]);
